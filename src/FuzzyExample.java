65c8fd97a85616c689906125c7125f08d12fab1b
SmartEken@gmail.com <SmartEken@gmail.com@3068088c-936d-0d82-f8bc-28e475f10423>
2011-05-05 13:49:43 +0000

    Fuzzy example with an fcl-file

---
+++ src/FuzzyExample.java
@@ -0,0 +1,35 @@
+import net.sourceforge.jFuzzyLogic.FIS;
+
+/**
+ * Test parsing an FCL file
+ * @author pcingola@users.sourceforge.net
+ */
+public class FuzzyExample {
+    public static void main(String[] args) throws Exception {
+        // Load from 'FCL' file
+        String fileName = "fcl/tipper.fcl";
+        FIS fis = FIS.load(fileName,true);
+        // Error while loading?
+        if( fis == null ) { 
+            System.err.println("Can't load file: '" 
+                                   + fileName + "'");
+            return;
+        }
+
+        // Show 
+        fis.chart();
+
+        // Set inputs
+        fis.setVariable("service", 3);
+        fis.setVariable("food", 7);
+
+        // Evaluate
+        fis.evaluate();
+
+        // Show output variable's chart 
+        fis.getVariable("tip").chartDefuzzifier(true);
+
+        // Print ruleSet
+        System.out.println(fis);
+    }
+}
