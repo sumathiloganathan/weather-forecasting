122a1e43fda2635829af8315055a9f776ec5c329
SmartEken@gmail.com <SmartEken@gmail.com@3068088c-936d-0d82-f8bc-28e475f10423>
2011-05-05 12:19:07 +0000

    Example of an XOR operator in EncogExample

---
+++ src/EncogExample.java
@@ -0,0 +1,62 @@
+import org.encog.ml.data.MLData;
+import org.encog.ml.data.MLDataPair;
+import org.encog.neural.data.NeuralData;
+import org.encog.neural.data.NeuralDataPair;
+import org.encog.neural.data.NeuralDataSet;
+import org.encog.neural.data.basic.BasicNeuralDataSet;
+import org.encog.neural.networks.BasicNetwork;
+import org.encog.neural.networks.layers.BasicLayer;
+import org.encog.neural.networks.training.Train;
+import org.encog.neural.networks.training.propagation.resilient.ResilientPropagation;
+import org.encog.util.logging.Logging;
+
+/**
+ * XOR: This example is essentially the "Hello World" of neural network
+ * programming. This example shows how to construct an Encog neural network to
+ * predict the output from the XOR operator. This example uses resilient
+ * propagation (RPROP) to train the neural network. RPROP is the best general
+ * purpose supervised training method provided by Encog.
+ */
+public class EncogExample {
+
+	public static double XOR_INPUT[][] = { { 0.0, 0.0 }, { 1.0, 0.0 },
+			{ 0.0, 1.0 }, { 1.0, 1.0 } };
+
+	public static double XOR_IDEAL[][] = { { 0.0 }, { 1.0 }, { 1.0 }, { 0.0 } };
+
+	public static void main(final String args[]) {
+
+		Logging.stopConsoleLogging();
+
+		final BasicNetwork network = new BasicNetwork();
+		network.addLayer(new BasicLayer(2));
+		network.addLayer(new BasicLayer(2));
+		network.addLayer(new BasicLayer(1));
+		network.getStructure().finalizeStructure();
+		network.reset();
+
+		final NeuralDataSet trainingSet = new BasicNeuralDataSet(
+				EncogExample.XOR_INPUT, EncogExample.XOR_IDEAL);
+
+		// train the neural network
+		final Train train = new ResilientPropagation(network, trainingSet);
+
+		int epoch = 1;
+
+		do {
+			train.iteration();
+			//System.out.println("Epoch #" + epoch + " Error:" + train.getError());
+			epoch++;
+		} while (train.getError() > 0.01);
+
+		// test the neural network
+		System.out.println("Neural Network Results:");
+		for (final MLDataPair pair : trainingSet) {
+			final MLData output = network.compute(pair.getInput());
+			System.out.println(pair.getInput().getData(0) + ","
+					+ pair.getInput().getData(1) + ", actual="
+					+ output.getData(0) + ",ideal="
+					+ pair.getIdeal().getData(0));
+		}
+	}
+}
