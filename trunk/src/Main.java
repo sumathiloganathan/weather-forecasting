0d72a7ca916ccf88b0fd41190ca505107970a7d1
erokan4@hotmail.com <erokan4@hotmail.com@3068088c-936d-0d82-f8bc-28e475f10423>
2011-05-11 10:16:48 +0000

    very simple neural network implementation, no fuzzy logic

--- trunk/src/Main.java
+++ trunk/src/Main.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 
-import net.sourceforge.*;
-import org.encog.*;
+import org.encog.ml.data.basic.BasicMLDataSet;
 
 public class Main {
 	
@@ -9,14 +8,18 @@ public class Main {
 		DataParser dp = new DataParser();
 		dp.parse("SMHI_3hours_clim_7142.txt", true);
 		System.out.println("number of entries:"+dp.size());
-		for (int j=0; j < 10; j++){
+		/*for (int j=0; j < 10; j++){
 			System.out.print("entry number:"+j+" date:"+dp.getDate(j)+ " time:"+dp.getTime(j));
 			double[] data = dp.getData(j);
 			for (int i=0; i<data.length; i++){
 				System.out.print(" "+DataParser.NAMES[i]+":"+data[i]);
 			}
 			System.out.println();
-		}
+		}*/
+		
+		NeuralNetwork nn = new NeuralNetwork();
+		
+		nn.train(new BasicMLDataSet(dp.asVerySimpleTrainingData()));
 		
 	}
 }
